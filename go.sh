d87169c62f71618895fb5639aa6fb27c7047e4fa
youaresoroman <qfaipkookw@groupoffice.ch>
2020-03-04 12:48:22 +0100

    KW 2020-03-04

--- go.sh
+++ go.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo source .venv/Scripts/activate
 python -m venv .venv
-source .venv/Scripts/activate
-python -m pip install --upgrade pip
-pip -V
 git config --global user.name "youaresoroman"
 git config --global user.email "qfaipkookw@groupoffice.ch"
+python -m pip install --upgrade pip
+pip -V
+pip install beautifulsoup4
+pip install requests
--- scrapy.py
+++ scrapy.py
@@ -1,16 +1,17 @@
-import scrapy
+from requests import get
+from bs4 import BeautifulSoup
+class Parser:
+    def __init__(self, url):
+        self.response = get(url)
+    def status(self):
+        return {
+            "status_code": self.response.status_code,
+            "timing": self.response.elapsed}
+    def parse(self):
+        html_soup = BeautifulSoup(self.response.text, 'html.parser')
+        job_elems = html_soup.find_all('ol', class_='pproduct-reviews js_reviews-hook')
+        return job_elems
 
-class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['https://blog.scrapinghub.com']
-
-    def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').get()}
-
-        for next_page in response.css('a.next-posts-link'):
-            yield response.follow(next_page, self.parse)
-
-spider = BlogSpider()
-print(spider.parse())
-print('ok')
+go = Parser("https://www.ceneo.pl/77397396")
+print(go.status())
+print(go.parse())
