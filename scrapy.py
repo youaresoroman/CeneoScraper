a98ff310cdf33d1a29ab1e599eb9d6924b1c3382
youaresoroman <qfaipkookw@groupoffice.ch>
2020-03-11 13:06:09 +0100

    hui

--- scrapy.py
+++ scrapy.py
@@ -1,17 +1,27 @@
 from requests import get
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bso
+import lxml
 class Parser:
     def __init__(self, url):
         self.response = get(url)
-    def status(self):
-        return {
-            "status_code": self.response.status_code,
-            "timing": self.response.elapsed}
+        self.parsed = None
+        self.status = self.response.status_code
+        if (self.status == 200):
+            self.parsed = self.parse()
+    def __str__(self):
+        return f"Status code: {self.status}"
     def parse(self):
-        html_soup = BeautifulSoup(self.response.text, 'html.parser')
-        job_elems = html_soup.find_all('ol', class_='pproduct-reviews js_reviews-hook')
-        return job_elems
+        return bso(self.response.text, 'lxml')
+    def getBlock(self, type, name):
+        return self.parsed.find_all(type, class_=name)
+    def GetParams(self, blocks, out=[]):
+        for block in blocks:
+            text = block.find('div', class_='reviewer-name-line"')
+            out.append(text.prettify())
+        return out
 
 go = Parser("https://www.ceneo.pl/77397396")
-print(go.status())
-print(go.parse())
+
+print(go)
+This = go.getBlock("li","review-box")
+print(go.GetParams(This))
