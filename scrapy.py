3a5f826b1b09c1ef3e3813f8d07c2a7c67849df7
debi <debi@debian.debian>
2020-03-18 16:22:32 -0700

    update

--- scrapy.py
+++ scrapy.py
@@ -1,27 +1,50 @@
 from requests import get
 from bs4 import BeautifulSoup as bso
-import lxml
-class Parser:
-    def __init__(self, url):
-        self.response = get(url)
+from json import dump
+
+class Ceneo:
+    def __init__(self, id):
+        self.response = get('https://www.ceneo.pl/' + id)
         self.parsed = None
         self.status = self.response.status_code
         if (self.status == 200):
             self.parsed = self.parse()
     def __str__(self):
-        return f"Status code: {self.status}"
+        return "Status code: {}".format(self.status)
     def parse(self):
-        return bso(self.response.text, 'lxml')
-    def getBlock(self, type, name):
-        return self.parsed.find_all(type, class_=name)
-    def GetParams(self, blocks, out=[]):
-        for block in blocks:
-            text = block.find('div', class_='reviewer-name-line"')
-            out.append(text.prettify())
-        return out
+        return bso(self.response.text, "html.parser")
+    def opinions(self, dictionary = {}):
+        opinions = self.parsed.select("li.review-box")
+        for opinion in opinions[0:6]:
+            author = opinion.find('div', {"class": 'reviewer-name-line'}).string
+            #accepted = opinion.find('div', {'class': 'product-review-pz'}).em.string
+            #date_out = opinion.findAll('time')[0].string
 
-go = Parser("https://www.ceneo.pl/77397396")
+            try:
+                pros = ", ".join(opinion.find('div', {'class': 'pros-cell'}).ul.get_text().strip().split("\n"))
+            except:
+                pros = None
 
-print(go)
-This = go.getBlock("li","review-box")
-print(go.GetParams(This))
+            try:
+                cons = ", ".join(opinion.find('div', {'class': 'cons-cell'}).ul.get_text().strip().split('\n'))
+            except:
+                cons = None
+
+            dictionary[author.replace("\n", "").replace(" ", "")] = {
+                'id': opinion["data-entry-id"],
+                'recommendation': opinion.find('em', {"class": "product-recommended"}).string,
+                'content': opinion.find('p', {'class': 'product-review-body'}).string,
+                'score': opinion.find('span', {'class': 'review-score-count'}).string,
+                'useful': opinion.find('button', {'class': 'vote-yes'}).span.string,
+                'useless': opinion.find('button', {'class': 'vote-no'}).span.string,
+                'buy_date': opinion.findAll('time')[1].string,
+                'cons': cons,
+                'pros': pros}
+        return dictionary
+
+product = Ceneo("77397396")
+
+print(product)
+if product.status == 200:
+    with open('Opinions.json', 'w', encoding='utf-8')as file:
+        dump(product.opinions(), file, ensure_ascii=False, indent=4)    
